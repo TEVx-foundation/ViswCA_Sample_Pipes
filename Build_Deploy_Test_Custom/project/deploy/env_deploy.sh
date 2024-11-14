85f4d8794f00206201c8abfb95ac9398b4ba4504
Viswam PC <viswampc143@gmail.com>
2024-11-14 12:40:01 +0530

    update::Commit

--- Build_Deploy_Test_Custom/project/deploy/env_deploy.sh
+++ Build_Deploy_Test_Custom/project/deploy/env_deploy.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo "Environment Deployment Began."
 touch env_deploy.txt
 echo echo "Completed" > env_deploy.txt
--- Build_Deploy_Test_Custom/project/env_init.sh
+++ Build_Deploy_Test_Custom/project/env_init.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo "Environment Initialisation Began."
 touch env_dependencies\env_setup.txt
 echo echo "Completed" > env_dependencies\env_setup.txt
